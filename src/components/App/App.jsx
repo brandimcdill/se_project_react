58ff5fc2ea52f5fa60446fbc242e18056b9c34a1
brandimcdill <mcdillbrandig@gmail.com>
2025-07-23 00:19:41 -0400

    finished basic set up and header

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import "../App/App.css";
+import Header from "../Header/header";
+
+function App() {
+  return (
+    <div className="page">
+      <div className="page__content">
+        <Header />
+      </div>
+    </div>
+  );
+}
+
+export default App;
