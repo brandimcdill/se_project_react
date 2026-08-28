c3a6b257049a678d3b747de06392c33c2923d5d3
Brandi McDill <mcdillbrandig@gmail.com>
2026-08-28 13:00:07 -0400

    fix: pass modal opening handlers down to ItemCard

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+import CurrentUserContext  from "../../contexts/CurrentUserContext";
 import "./ItemCard.css";
 
-function ItemCard({ item, onClick }) {
+function ItemCard({ item, onClick, onCardLike, isLoggedIn }) {
+  const currentUser = useContext(CurrentUserContext);
+
   const handleCardClick = () => {
     onClick(item);
+  };
+
+  const isLiked = item.likes?.some((id) => id === currentUser?._id);
+
+  const itemLikeButtonClassName = `card__like-btn ${isLiked ? "card__like-btn_active" : ""}`;
+
+  const handleLike = () => {
+    onCardLike({ id: item._id, isLiked});
   };
 
   let nameValue;
@@ -14,7 +26,17 @@ function ItemCard({ item, onClick }) {
 
   return (
     <li className="card">
+      <div className="card__header">
       <h2 className="card__name">{nameValue}</h2>
+      {isLoggedIn && (
+        <button 
+        type="button"
+        className={itemLikeButtonClassName}
+        onClick={handleLike}
+        aria-label="Like item"
+        />
+      )}
+      </div>
       <img
         onClick={handleCardClick}
         className="card__image"
